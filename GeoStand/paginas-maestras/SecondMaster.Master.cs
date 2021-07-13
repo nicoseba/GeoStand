bfe69a95a62b88eeb9812ae226ea4e9e7ed0a437
Nicolas Hernandez <nicogonza2308@gmail.com>
2021-07-13 16:46:34 -0400

    Add links en 2doMaster

--- GeoStand/paginas-maestras/SecondMaster.Master.cs
+++ GeoStand/paginas-maestras/SecondMaster.Master.cs
@@ -13,5 +13,10 @@ namespace GeoStand.paginas_maestras
         {
 
         }
+
+        protected void LnkPublication_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("busquedaPublicaciones.aspx");
+        }
     }
 }
--- GeoStand/paginas-maestras/SecondMaster.Master.designer.cs
+++ GeoStand/paginas-maestras/SecondMaster.Master.designer.cs
@@ -33,6 +33,24 @@ namespace GeoStand.paginas_maestras
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control LnkPublication.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LnkPublication;
+
+        /// <summary>
+        /// Control Lnk.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton Lnk;
+
+        /// <summary>
         /// Control CphContent.
         /// </summary>
         /// <remarks>
